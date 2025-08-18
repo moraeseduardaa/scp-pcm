c1a3ba0eeb7e6421c306ca56bae08be8cf62212f
eduarda-moraes <moraeseduarda72@gmail.com>
2025-08-18 08:54:26 -0300

    Nova Tela de Login para Cadastros e Controles

--- src/components/loginPcm.vue
+++ src/components/loginPcm.vue
@@ -8,24 +8,27 @@
               <h2 class="fw-bold mb-2" style="color: #000080;">Informe seu login do PCM</h2>
               <div class="mx-auto mb-5" style="height: 4px; width: 80%; background: #dc3545;"></div>
               <div class="mb-4">
-                <input v-model="usuario" type="text" class="form-control form-control-lg mb-3"
-                  placeholder="Usuário" style="background-color: #f8f9fa; color: #000; border: 2px solid #dc3545;"
+                <input v-model="usuario" type="text" class="form-control form-control-lg mb-3" placeholder="Usuário"
+                  style="background-color: #f8f9fa; color: #000; border: 2px solid #dc3545;"
                   @input="usuario = usuario.toUpperCase()">
-                <input v-model="senha" type="password" class="form-control form-control-lg"
-                  placeholder="Senha" style="background-color: #f8f9fa; color: #000; border: 2px solid #dc3545;">
+                <input v-model="senha" type="password" class="form-control form-control-lg" placeholder="Senha"
+                  style="background-color: #f8f9fa; color: #000; border: 2px solid #dc3545;">
+              </div>
+              <div v-if="erroLogin" class="mb-3 text-danger">{{ erroLogin }}</div>
+              
+              <div class="buttons">
+                <button class="btn btn-secondary px-5 mt-3 me-2" @click="emit('voltar-login')"
+                  style="border-radius: 50px; padding: 10px 12px; font-size: 17px;">
+                  <i class="bi bi-arrow-left-circle me-2"></i>
+                  Voltar
+                </button>
+                <button class="btn btn-danger px-5 mt-3" @click="fazerLogin" :disabled="loading"
+                  style="border-radius: 50px; padding: 10px 12px; font-size: 17px;">
+                  <i class="bi bi-arrow-right-circle me-2"></i>
+                  Login
+                </button>
               </div>
 
-              <div v-if="erroLogin" class="mb-3 text-danger">{{ erroLogin }}</div>
-
-              <button class="btn btn-secondary btn-lg px-5 mt-3 me-2" @click="emit('voltar-login')" style="border-radius: 50px;">
-                <i class="bi bi-arrow-left-circle me-2"></i>
-                Voltar
-              </button>
-              <button class="btn btn-danger btn-lg px-5 mt-3 ml-3" @click="fazerLogin" :disabled="loading"
-                style="border-radius: 50px;">
-                <i class="bi bi-arrow-right-circle me-2"></i>
-                Login
-              </button>
             </div>
           </div>
         </div>
